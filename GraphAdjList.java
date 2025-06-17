fce56cec531bfb14b7235174a3bd566cbe9fae45
SURENDRANATH M <168511637+surendranathm5@users.noreply.github.com>
2025-06-17 10:33:39 +0530

    Create GraphAdjList.java

---
+++ GraphAdjList.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class GraphAdjList {
+    private Map<Integer, List<Integer>> adjList = new HashMap<>();
+
+    public void addEdge(int u, int v) {
+        adjList.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+        adjList.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+    }
+
+    public void printGraph() {
+        for (int node : adjList.keySet()) {
+            System.out.print(node + ": ");
+            for (int neighbor : adjList.get(node)) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphAdjList graph = new GraphAdjList();
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.printGraph();
+    }
+}
