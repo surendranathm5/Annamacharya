94dc3ded0de244082d22e371fa0b7d35cc785a31
SURENDRANATH M <168511637+surendranathm5@users.noreply.github.com>
2025-06-17 10:33:15 +0530

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Graph {
+    private Map<Integer, List<Integer>> adjList;
+
+    public Graph() {
+        adjList = new HashMap<>();
+    }
+
+    public void addEdge(int src, int dest) {
+        adjList.putIfAbsent(src, new ArrayList<>());
+        adjList.putIfAbsent(dest, new ArrayList<>());
+        adjList.get(src).add(dest);
+        adjList.get(dest).add(src);
+    }
+
+    public void removeEdge(int src, int dest) {
+        if (adjList.containsKey(src)) {
+            adjList.get(src).remove(Integer.valueOf(dest));
+        }
+        if (adjList.containsKey(dest)) {
+            adjList.get(dest).remove(Integer.valueOf(src));
+        }
+    }
+
+    public void printGraph() {
+        for (Map.Entry<Integer, List<Integer>> entry : adjList.entrySet()) {
+            System.out.print(entry.getKey() + " -> ");
+            for (int neighbor : entry.getValue()) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.printGraph();
+        graph.removeEdge(1, 3);
+        graph.printGraph();
+    }
+}
