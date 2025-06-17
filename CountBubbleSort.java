c534d3bbac7c3a9e8317ae75227eaed0ee497abd
SURENDRANATH M <168511637+surendranathm5@users.noreply.github.com>
2025-06-17 10:29:55 +0530

    Create CountBubbleSort.java

---
+++ CountBubbleSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class CountBubbleSort {
+	static void bubbleSort(int[] arr) {
+        int n = arr.length;
+		int count=0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) 
+			{
+                if (arr[j] > arr[j + 1]) 
+				{
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+					count++;
+                }
+			}
+        }
+		System.out.println("Total swaps : "+count);
+    }
+    public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter Number Of Elements are need to sort");
+		int dig=input.nextInt();
+        int[] arr=new int[dig];
+		System.out.print("Enter the Elements");
+		for(int i=0;i<dig;i++)
+		{
+			arr[i]=input.nextInt();
+		}
+		System.out.println("Before Sorting");
+		for(int num:arr)System.out.print(num+" ");
+        bubbleSort(arr);
+		System.out.println("\nAfter sorting");
+        for (int num : arr)
+		{
+            System.out.print(num + " ");
+        }
+    }
+}
